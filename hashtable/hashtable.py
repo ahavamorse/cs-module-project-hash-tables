76039321f6b6ed55d4ee8644070cd7676124f68e
ahavamorse <ahavamorse@icloud.com>
2021-05-06 14:44:40 -0700

    Fixed resize function to only rehash items that exist and to iterate properly through each linked list

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -69,7 +69,7 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        self.values = [HashTableEntry(None, None)] * capacity
+        self.values = [None] * capacity
         self.capacity = capacity
         self.num_of_items = 0
 
@@ -215,13 +215,15 @@ class HashTable:
         """
 
         old_values = self.values
-        self.values = [HashTableEntry(None, None)] * new_capacity
+        self.values = [None] * new_capacity
         self.capacity = new_capacity
         for item in old_values:
-            curr_item = item
-            while item.next:
-                self.put(curr_item.key, item.value)
-                curr_item = curr_item.next
+            if item:
+                self.put(item.key, item.value)
+                curr_item = item
+                while curr_item.next:
+                    self.put(curr_item.next.key, curr_item.next.value)
+                    curr_item = curr_item.next
 
 
 # if __name__ == "__main__":
@@ -258,199 +260,3 @@ class HashTable:
 #         print(ht.get(f"line_{i}"))
 #
 #     print("")
-
-import unittest
-
-
-class TestHashTable(unittest.TestCase):
-
-    def test_hash_table_insertion_and_retrieval(self):
-        ht = HashTable(8)
-
-        ht.put("key-0", "val-0")
-        ht.put("key-1", "val-1")
-        ht.put("key-2", "val-2")
-        ht.put("key-3", "val-3")
-        ht.put("key-4", "val-4")
-        ht.put("key-5", "val-5")
-        ht.put("key-6", "val-6")
-        ht.put("key-7", "val-7")
-        ht.put("key-8", "val-8")
-        ht.put("key-9", "val-9")
-
-        return_value = ht.get("key-0")
-        self.assertTrue(return_value == "val-0")
-        return_value = ht.get("key-1")
-        self.assertTrue(return_value == "val-1")
-        return_value = ht.get("key-2")
-        self.assertTrue(return_value == "val-2")
-        return_value = ht.get("key-3")
-        self.assertTrue(return_value == "val-3")
-        return_value = ht.get("key-4")
-        self.assertTrue(return_value == "val-4")
-        return_value = ht.get("key-5")
-        self.assertTrue(return_value == "val-5")
-        return_value = ht.get("key-6")
-        self.assertTrue(return_value == "val-6")
-        return_value = ht.get("key-7")
-        self.assertTrue(return_value == "val-7")
-        return_value = ht.get("key-8")
-        self.assertTrue(return_value == "val-8")
-        return_value = ht.get("key-9")
-        self.assertTrue(return_value == "val-9")
-
-    def test_hash_table_pution_overwrites_correctly(self):
-        ht = HashTable(8)
-
-        ht.put("key-0", "val-0")
-        ht.put("key-1", "val-1")
-        ht.put("key-2", "val-2")
-        ht.put("key-3", "val-3")
-        ht.put("key-4", "val-4")
-        ht.put("key-5", "val-5")
-        ht.put("key-6", "val-6")
-        ht.put("key-7", "val-7")
-        ht.put("key-8", "val-8")
-        ht.put("key-9", "val-9")
-
-        ht.put("key-0", "new-val-0")
-        ht.put("key-1", "new-val-1")
-        ht.put("key-2", "new-val-2")
-        ht.put("key-3", "new-val-3")
-        ht.put("key-4", "new-val-4")
-        ht.put("key-5", "new-val-5")
-        ht.put("key-6", "new-val-6")
-        ht.put("key-7", "new-val-7")
-        ht.put("key-8", "new-val-8")
-        ht.put("key-9", "new-val-9")
-
-        return_value = ht.get("key-0")
-        self.assertTrue(return_value == "new-val-0")
-        return_value = ht.get("key-1")
-        self.assertTrue(return_value == "new-val-1")
-        return_value = ht.get("key-2")
-        self.assertTrue(return_value == "new-val-2")
-        return_value = ht.get("key-3")
-        self.assertTrue(return_value == "new-val-3")
-        return_value = ht.get("key-4")
-        self.assertTrue(return_value == "new-val-4")
-        return_value = ht.get("key-5")
-        self.assertTrue(return_value == "new-val-5")
-        return_value = ht.get("key-6")
-        self.assertTrue(return_value == "new-val-6")
-        return_value = ht.get("key-7")
-        self.assertTrue(return_value == "new-val-7")
-        return_value = ht.get("key-8")
-        self.assertTrue(return_value == "new-val-8")
-        return_value = ht.get("key-9")
-        self.assertTrue(return_value == "new-val-9")
-
-    def test_hash_table_removes_correctly(self):
-        ht = HashTable(8)
-
-        ht.put("key-0", "val-0")
-        ht.put("key-1", "val-1")
-        ht.put("key-2", "val-2")
-        ht.put("key-3", "val-3")
-        ht.put("key-4", "val-4")
-        ht.put("key-5", "val-5")
-        ht.put("key-6", "val-6")
-        ht.put("key-7", "val-7")
-        ht.put("key-8", "val-8")
-        ht.put("key-9", "val-9")
-
-        return_value = ht.get("key-0")
-        self.assertTrue(return_value == "val-0")
-        return_value = ht.get("key-1")
-        self.assertTrue(return_value == "val-1")
-        return_value = ht.get("key-2")
-        self.assertTrue(return_value == "val-2")
-        return_value = ht.get("key-3")
-        self.assertTrue(return_value == "val-3")
-        return_value = ht.get("key-4")
-        self.assertTrue(return_value == "val-4")
-        return_value = ht.get("key-5")
-        self.assertTrue(return_value == "val-5")
-        return_value = ht.get("key-6")
-        self.assertTrue(return_value == "val-6")
-        return_value = ht.get("key-7")
-        self.assertTrue(return_value == "val-7")
-        return_value = ht.get("key-8")
-        self.assertTrue(return_value == "val-8")
-        return_value = ht.get("key-9")
-        self.assertTrue(return_value == "val-9")
-
-        ht.delete("key-9")
-        ht.delete("key-8")
-        ht.delete("key-7")
-        ht.delete("key-6")
-        ht.delete("key-5")
-        ht.delete("key-4")
-        ht.delete("key-3")
-        ht.delete("key-2")
-        ht.delete("key-1")
-        ht.delete("key-0")
-
-        return_value = ht.get("key-0")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-1")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-2")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-3")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-4")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-5")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-6")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-7")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-8")
-        self.assertTrue(return_value is None)
-        return_value = ht.get("key-9")
-        self.assertTrue(return_value is None)
-
-    def test_hash_table_resize(self):
-        ht = HashTable(8)
-
-        ht.put("key-0", "val-0")
-        ht.put("key-1", "val-1")
-        ht.put("key-2", "val-2")
-        ht.put("key-3", "val-3")
-        ht.put("key-4", "val-4")
-        ht.put("key-5", "val-5")
-        ht.put("key-6", "val-6")
-        ht.put("key-7", "val-7")
-        ht.put("key-8", "val-8")
-        ht.put("key-9", "val-9")
-
-        ht.resize(1024)
-
-        self.assertTrue(ht.get_num_slots() == 1024)
-
-        return_value = ht.get("key-0")
-        self.assertTrue(return_value == "val-0")
-        return_value = ht.get("key-1")
-        self.assertTrue(return_value == "val-1")
-        return_value = ht.get("key-2")
-        self.assertTrue(return_value == "val-2")
-        return_value = ht.get("key-3")
-        self.assertTrue(return_value == "val-3")
-        return_value = ht.get("key-4")
-        self.assertTrue(return_value == "val-4")
-        return_value = ht.get("key-5")
-        self.assertTrue(return_value == "val-5")
-        return_value = ht.get("key-6")
-        self.assertTrue(return_value == "val-6")
-        return_value = ht.get("key-7")
-        self.assertTrue(return_value == "val-7")
-        return_value = ht.get("key-8")
-        self.assertTrue(return_value == "val-8")
-        return_value = ht.get("key-9")
-        self.assertTrue(return_value == "val-9")
-
-
-if __name__ == '__main__':
-    unittest.main()
