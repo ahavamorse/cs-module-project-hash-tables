5125ab7da0d0575b703c345d2985831b63c343ca
ahavamorse <ahavamorse@icloud.com>
2021-05-10 13:45:43 -0700

    Implemeted improved expensive sequence function using a lookup table

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,17 @@
-# Your code here
-
+lookup_table = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    if (x, y, z) in lookup_table:
+        return lookup_table[(x, y, z)]
+    else:
+        if x <= 0:
+            value = y + z
+            lookup_table[(x, y, z)] = value
+            return value
+        if x > 0:
+            value = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+            lookup_table[(x, y, z)] = value
+            return value
 
 
 
