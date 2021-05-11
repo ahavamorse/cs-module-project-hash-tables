1f2a9c4c64b8752baea9708e18fd6e8bff2b481a
ahavamorse <ahavamorse@icloud.com>
2021-05-11 08:48:00 -0700

    Implemented random sentence generator using a dictionary and random next word choices

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,43 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+def is_start_word(word):
+    first_letter = word[0]
+    return first_letter.isupper()
+
+dict = {}
+start_words = []
+words = words.split()
+for i in range(0, len(words)):
+    word = words[i]
+
+    if is_start_word(word):
+        start_words.append(word)
+    if i < len(words) - 1:
+        next_word = words[i + 1]
+
+        if word in dict:
+            dict[word].append(next_word)
+        else:
+            dict[word] = [next_word]
 
 
 # TODO: construct 5 random sentences
-# Your code here
+def is_not_stop_word(word):
+    last_char = word[-1]
+    if last_char == '"':
+        last_char = word[-2]
+    if last_char == "." or last_char == "?" or last_char == "!":
+        return False
+    else:
+        return True
 
+
+for i in range(1, 6):
+    sentence_words = []
+    curr_word = random.choice(start_words)
+    while is_not_stop_word(curr_word):
+        print(curr_word, end=" ")
+        possible_words = dict[curr_word]
+        curr_word = random.choice(possible_words)
+    print(curr_word)
