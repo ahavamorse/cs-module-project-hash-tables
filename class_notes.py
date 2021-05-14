a4d72830ff4502bda154eafea313cce6dbfd5fb0
ahavamorse <ahavamorse@icloud.com>
2021-05-14 10:05:33 -0700

    Added notes and code with two different problems that were optimized with dictionaries and sets

--- class_notes.py
+++ class_notes.py
@@ -19,6 +19,7 @@ records = [
     ('Jordan', 'Marketing')
 ]
 
+
 # Need to print all departments
 # Need to print everyone in Engineering
 
@@ -44,6 +45,7 @@ def build_index(recs):
 
     return index
 
+
 department_index = build_index(records)
 
 print(department_index.keys())
@@ -64,8 +66,10 @@ def build_sqrt_lookup_table():
         lookup_table[i] = get_inverse_square(i)
     return lookup_table
 
+
 def get_inverse_square(num):
     return 1 / math.sqrt(num)
+
 
 sqrt_lookup_table = build_sqrt_lookup_table()
 
@@ -91,6 +95,7 @@ def letter_count(s):
             dict[char] += 1
     return dict
 
+
 print(letter_count("aaabbc"))
 print(letter_count("Hello!"))
 print(letter_count("The quick brown fox jumps over the lazy dogs"))
@@ -107,12 +112,12 @@ def double_letter(s):
         else:
             return char
 
-print(double_letter("abcdefghijklmnopqrstuvwxyz a"))
 
+print(double_letter("abcdefghijklmnopqrstuvwxyz a"))
 
 # Dictionaries are very useful in substitution ciphers (transfer data from one thing to another)
 
-encode_table = {
+code_table = {
     'A': 'Z',
     'B': 'Y',
     'C': 'X',
@@ -141,6 +146,7 @@ encode_table = {
     'Z': 'A'
 }
 
+
 def encode(plain_text):
     cipher = ""
 
@@ -148,8 +154,9 @@ def encode(plain_text):
         if char.isspace():
             cipher += ' '
         else:
-            cipher += encode_table[char]
+            cipher += code_table[char]
     return cipher
+
 
 def decode(cipher_text):
     plain_text = ''
@@ -157,8 +164,77 @@ def decode(cipher_text):
         if char.isspace():
             plain_text += ' '
         else:
-            plain_text += encode_table[char]
+            plain_text += code_table[char]
     return plain_text
+
 
 print(encode("DICTIONARIES ARE AWESOME"))
 print(decode("WRXGRLMZIRVH ZIV ZDVHLNV"))
+
+
+# Day Four Class Notes
+
+# Jewels and Stones
+# List of jewels and stones are passed in
+# Find out how many of the stones are jewels
+
+# Naive solution:
+# Use nested loops to compare each jewel and each stone
+
+# My Proposed solution:
+# Create dictionary to count number of each type of stone
+# Then go through the jewels and add up the number of each among the stones
+# Dictionary:
+#   Keys: char from stones
+#   Values: # of occurrences
+# Then for each char in jewels how many times is it in stones?
+# So how many of each jewel do we have? Add up the totals for result
+
+# Better idea:
+# Go through char in jewels create a key with value 0
+# Then for char in stones, if it is in the dict, increment the value
+# Last add up values in dict to get total num of jewels
+
+
+def numJewelsInStones(self, jewels, stones) -> int:
+    jewel_count = 0
+    jewels_dict = {}
+    for jewel in jewels:
+        jewels_dict[jewel] = 0
+
+    for stone in stones:
+        if stone in jewels_dict:
+            jewel_count += 1
+
+    return jewel_count
+
+
+# print(numJewelsInStones("", ""))
+
+
+# Happy Number
+# input is a number, output is a bool
+# add up the squares of each digit of the number
+# do so over and over until you get 1 or repeat the same number
+
+# My Proposed Solution:
+# Find sum of square of each digit in number
+# if result is one return true or if we've already seen number return false
+# otherwise store number in set and repeat process
+
+seen_values = set()
+
+def isHappy(self, n) -> bool:
+
+    if n == 1:
+        return True
+    elif n in seen_values:
+        return False
+    else:
+        result = 0
+        for digit in str(n):
+            result += int(digit) ** 2
+        seen_values.add(n)
+        return isHappy(result)
+
+print(isHappy(n))
